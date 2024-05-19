9676bf1700b35bb159b4d6efda512d35e276c4e6
Ehsan <ehsanzeinalinia@gmail.com>
2024-05-19 17:22:52 +0330

    remove some unnecessary files

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,3 +159,23 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+function sendMail() {
+  const username = document.getElementById("user_name_inp").value;
+  const userMail = document.getElementById("user_email_inp").value
+  const userDesc = document.getElementById("user_desc_inp").value
+  console.log(username);
+
+  emailjs.send('service_sh4om3q', 'template_hcbl6ph', { user_name: username, user_description: userDesc, user_email: userMail }).then(
+      function (response) {
+
+          console.log('SUCCESS!', response.status, response.text);
+      },
+      function (err) {
+          console.log('FAILED...', err);
+      },
+  );
+  document.getElementById("user_name_inp").value = ""
+  document.getElementById("user_email_inp").value = ""
+  document.getElementById("user_desc_inp").value = ""
+}
